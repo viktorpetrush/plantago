d3ef070f40ec0e22024633d39e10496a5ce0061c
viktorpetrush <viktor.petrushevuch@gmail.com>
2017-03-24 17:10:17 +0200

    wrote apparat controller

--- app/controllers/apparats_controller.rb
+++ app/controllers/apparats_controller.rb
@@ -1,5 +1,51 @@
 class ApparatsController < ApplicationController
+  
+  before_action :set_apparat, only: [:show, :edit, :update, :destroy]
+  
   def index
     @apparats = Apparat.all
   end
+
+  def new
+    @apparat = Apparat.new
+  end
+
+  def edit
+  end
+
+  def create
+    @apparat = Apparat.new(apparat_params)
+
+    if @apparat.save
+      redirect_to @apparat
+      flash[:notice] = "Apparat was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @apparat.update(apparat_params)
+      redirect_to @apparat
+      flash[:notice] = "Apparat was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @apparat.destroy
+    redirect_to apparats_path
+    ulash[:notice] = "Apparat was successfully deleted."
+  end
+
+  private
+
+    def set_apparat
+      @apparat = Apparat.find(params[:id])
+    end
+
+    def apparat_params
+      params.requiure(:apparat).permit(:name)
+    end
 end
