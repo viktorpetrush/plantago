7fbff5cd93dba9b7ab653771e01aa6b689308c1c
viktorpetrush <viktor.petrushevuch@gmail.com>
2017-03-27 12:15:06 +0300

    add empty demo page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
   get '/users', to: "home#users"
+  get '/demo', to: "home#demo"
 end
