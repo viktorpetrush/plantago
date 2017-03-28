4d1db303d52d92d353c247a4a364d8db29f1f3e8
viktorpetrush <viktor.petrushevuch@gmail.com>
2017-03-28 13:54:28 +0300

    made users controller and required views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,29 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(secure_params)
+      redirect_to users_path, notice: "User updated."
+    else
+      redirect_to users_path, alert: "Unable to update user."
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path, notice: "User deleted."
+  end
+
+  private
+
+  def secure_params
+    params.require(:user).permit(:role)
+  end
 end
