94d013b703a7254688b5af1d58b025810b24436f
viktorpetrush <viktor.petrushevuch@gmail.com>
2017-04-05 15:11:16 +0300

    add enum type to Apparat

--- app/models/apparat.rb
+++ app/models/apparat.rb
@@ -1,3 +1,6 @@
 class Apparat < ApplicationRecord
   validates :name, presence: true
+  enum product_type: [:phones, :computer_laptop, :server, :thin_client, :router, :switch, 
+              :printer_scanner_mfp, :internet_settings, :other_information]
+  belongs_to :company
 end
