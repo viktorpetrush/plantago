6e8bc854a1fab638ee281696244bb1a2d945c692
viktorpetrush <viktor.petrushevuch@gmail.com>
2017-04-19 09:44:32 +0300

    add dependent: :destroy to models

--- app/models/apparat.rb
+++ app/models/apparat.rb
@@ -3,6 +3,6 @@ class Apparat < ApplicationRecord
   enum product_type: [:phones, :computer_laptop, :server, :thin_client, :router, :switch, 
                       :printer_scanner_mfp, :internet_settings, :other_information]
   belongs_to :company
-  has_many :apparats_permits
+  has_many :apparats_permits, dependent: :destroy
   has_many :users, through: :apparats_permits
 end
