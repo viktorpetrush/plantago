9cb8dfcea3386cdb2f6ccbeed3973ed09afc7726
viktorpetrush <viktor.petrushevuch@gmail.com>
2017-04-24 12:55:28 +0300

    when we add company role apparats roles add automatically with same roles

--- app/controllers/companies_permits_controller.rb
+++ app/controllers/companies_permits_controller.rb
@@ -7,7 +7,8 @@ class CompaniesPermitsController < ApplicationController
     @companies_permit = @user.companies_permits.build(companies_permit_params)
     authorize @companies_permit
     if @companies_permit.save
-      @user.apparats << @companies_permit.company.apparats - @user.apparats
+      #@user.apparats << @companies_permit.company.apparats - @user.apparats
+      create_apparat_role
       flash[:notice] = "User's role was successfully created."
       redirect_to @user
     else
@@ -16,9 +17,11 @@ class CompaniesPermitsController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:user_id])
     @companies_permit = CompaniesPermit.find(params[:id])
     authorize @companies_permit
     if @companies_permit.update_attributes(companies_permit_params)
+      update_apparats_role(@user, @companies_permit)
       flash[:notice] = "User's role was successfully updated"
       redirect_to @companies_permit.user
     else
@@ -38,6 +41,20 @@ class CompaniesPermitsController < ApplicationController
   end
 
   private
+
+    def create_apparat_role
+      @companies_permit.company.apparats.map do |apparat|
+        @user.apparats_permits.create(apparat: apparat, role: @companies_permit.role)
+      end
+    end
+
+    def update_apparats_role(user, company_permit)
+      company = company_permit.company
+      role = company_permit.role
+      user.apparats.where(company: company).map do |apparat|
+        user.apparats_permits.find_by(apparat: apparat).role = role
+      end
+    end
 
     def companies_permit_params
       params.require(:companies_permit).permit(:user_id, :company_id, :role)
