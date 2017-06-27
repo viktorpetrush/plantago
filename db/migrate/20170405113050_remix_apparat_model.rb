4a5ddfef3244ae0fa178bc2828d17eb15b9fd54c
viktorpetrush <viktor.petrushevuch@gmail.com>
2017-06-27 11:11:35 +0300

    change migration proper to MySQL

--- db/migrate/20170405113050_remix_apparat_model.rb
+++ db/migrate/20170405113050_remix_apparat_model.rb
@@ -2,6 +2,6 @@ class RemixApparatModel < ActiveRecord::Migration[5.0]
   def change
     remove_column :apparats, :company, :string
     add_index :apparats, :ip_address
-    add_reference :apparats, :company, foreign_key: true
+    add_reference :apparats, :company
   end
 end
